eee78708f35600acb4c07358239a2d38e91d1ff7
Nemospawn <75135682+RafaNoble@users.noreply.github.com>
2020-12-18 20:50:24 +0100

    Añadido tratamiento de errores

--- practica2.2/ejercicio12.cpp
+++ practica2.2/ejercicio12.cpp
@@ -13,6 +13,14 @@ int main() {
 
     file.open(pTemp.c_str());
 
+    if (!file.is_open()) {
+
+        cout << "No se pudo abrir el fichero\n";
+
+        return -1;
+
+    }
+
     for (int i = 1; i <= 5; ++i)
         file << "Linea " << i << '\n';
 
