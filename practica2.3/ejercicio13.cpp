fea2b5d7861bf9d06e754f6e72cddfe032d6222a
Nemospawn <75135682+RafaNoble@users.noreply.github.com>
2021-01-05 13:03:53 +0100

    Código modificado
    De acuerdo a las indicaciones del profesor.

--- practica2.3/ejercicio13.cpp
+++ practica2.3/ejercicio13.cpp
@@ -1,38 +1,60 @@
-#include <iostream>
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 using namespace std;
 
+void sigalrm_handler(int signal){
+
+    unlink("ejercicio13");
+
+}
+
+void sigusr1_handler(int signal){
+
+    return;
+
+}
+
 int main() {
 
     int sec;
     sigset_t signals;
-    pid_t pid;
+    struct sigaction sigalrm_act, sigusr1_act;
 
     cin >> sec;
 
-    pid = fork();
+    sigemptyset(&signals);
+    sigdelset(&signals, SIGALRM);
+    sigdelset(&signals, SIGUSR1);
 
-    switch (pid) {
+    sigalrm_act.sa_handler = sigalrm_handler;
+    sigusr1_act.sa_handler = sigusr1_handler;
 
-        case -1:
-        perror("fork");
+    sigemptyset(&sigalrm_act.sa_mask);
+    sigemptyset(&sigusr1_act.sa_mask);
+
+    sigalrm_act.sa_flags = 0;
+    sigusr1_act.sa_flags = 0;
+
+    if (sigaction(SIGALRM, &sigalrm_act, NULL) == -1) {
+
+        perror("Error");
+
         return -1;
 
-        case 0:
-        sigemptyset(&signals);
-        sigsuspend(&signals);
-        kill(getppid(), SIGTERM);
-        break;
+    }
 
-        default:
-        sleep(sec);
-        unlink("ejercicio13");
-        kill(pid, SIGTERM);
-        break;
+    if (sigaction(SIGUSR1, &sigusr1_act, NULL) == -1) {
+
+        perror("Error");
+
+        return -1;
 
     }
+
+    alarm(sec);
+
+    sigsuspend(&signals);
 
     return 0;
 
