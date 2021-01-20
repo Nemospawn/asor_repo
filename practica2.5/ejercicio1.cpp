f154b2a0d1ab9b1e30d0119de9525b85fd85518c
Nemospawn <75135682+RafaNoble@users.noreply.github.com>
2021-01-20 21:40:21 +0100

    Ejercicio 1 completado

---
+++ practica2.5/ejercicio1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+using namespace std;
+
+const int SIZE = 1000;
+
+int main() {
+
+    int info;
+    string hostname;
+    struct addrinfo hints, *result, *rp, *hostinfo;
+    char host[SIZE];
+
+    cin >> hostname;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+
+    info = getaddrinfo(hostname.c_str(), NULL, &hints, &result);
+    
+    if (info != 0) {
+
+        cout << "Error getaddrinfo(): " << gai_strerror(info) << '\n';
+
+        return -1;
+
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+
+        getnameinfo(rp->ai_addr, rp->ai_addrlen, host, sizeof(host), NULL, 0, NI_NUMERICHOST);
+
+        cout << host << "    " << rp->ai_family << "    " << rp->ai_socktype << '\n';
+
+    }
+
+    freeaddrinfo(result);
+
+    return 0;
+
+}
